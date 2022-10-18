65d58c6847530f4cf49a0933db56d2607beb2beb
fitareq <fitareq@gmail.com>
2022-10-18 23:30:15 +0600

    created -design: login.
    by: Forhadul

--- app/src/main/java/com/fitareq/oldbookstore/ui/login/LoginViewModel.java
+++ app/src/main/java/com/fitareq/oldbookstore/ui/login/LoginViewModel.java
@@ -1,4 +1,13 @@
 package com.fitareq.oldbookstore.ui.login;
 
-public class LoginViewModel {
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.ViewModel;
+
+public class LoginViewModel extends AndroidViewModel {
+    public LoginViewModel(@NonNull Application application) {
+        super(application);
+    }
 }
